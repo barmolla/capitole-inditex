dbcc1b2acf7d11cbda35d3bbfe9c2303b754f9f5
Brian Armolla <brian.armolla@gmail.com>
2023-09-04 17:51:33 -0300

    Handle exceptions related to invalid payloads

--- src/main/java/com/capitoleconsulting/testinditex/exception/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/capitoleconsulting/testinditex/exception/CustomResponseEntityExceptionHandler.java
@@ -2,16 +2,16 @@ package com.capitoleconsulting.testinditex.exception;
 
 import java.util.Date;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-@RestController
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
@@ -23,4 +23,11 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
 	public final ResponseEntity<Exception> handleRuntimeException(RuntimeException ex) {
 	    return new ResponseEntity<Exception>(ex, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		return new ResponseEntity<>(new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false)), HttpStatus.BAD_REQUEST);
+	}
+
 }
